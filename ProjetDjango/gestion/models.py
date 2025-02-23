5466d3c6f231f61aa716c3de020c2e9f7d8284e6
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-02-23 20:46:59 +0100

    Added UserManager

--- ProjetDjango/gestion/models.py
+++ ProjetDjango/gestion/models.py
@@ -1,11 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Ceci doit etre instalé:
 #python -m pip install Pillow
 #pip install django-phonenumber-field
 #pip install phonenumbers
+
+
+""" Manager personnalisé pour le modèle User sans champ username """
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("L'adresse email est obligatoire")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Le superutilisateur doit avoir is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Le superutilisateur doit avoir is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
 
@@ -35,6 +60,8 @@ class User(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return f"{self.get_full_name()} ({self.email})"
