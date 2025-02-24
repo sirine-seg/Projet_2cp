66eaaf5fdf3393392fdf1e2d3563e58fbf9275c3
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-02-24 17:30:51 +0100

    Update views.py

--- projetDjango/myDjango/gestion/views.py
+++ projetDjango/myDjango/gestion/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets,filters
-from django_filters.rest_framework import DjangoFilterBackend
+#from django_filters.rest_framework import DjangoFilterBackend
 from .models import Equipement
 from .serializers import EquipementSerializer
 
