8ab8cee1684471a739fa9198554762bfdfcb33ee
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-04-18 15:31:17 +0100

    Update badge.jsx

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,10 +1,11 @@
 const badge = ({ 
     text, 
     bgColor,
+    className="",
   }) => {
     return (
       <span
-        className="px-3 py-1 rounded-full font-bold text-[10px] text-[#F4F4F4]"
+        className={`px-3 py-1 rounded-full font-bold text-[12px] text-[#F4F4F4] ${className} `}
         style={{
           backgroundColor: bgColor,
         }}
