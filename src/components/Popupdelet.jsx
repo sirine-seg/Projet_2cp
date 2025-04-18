d7c185f5ea3f5582e91aedfadda8fa305c820ddf
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-18 16:13:04 +0200

    Add files via upload

---
+++ src/components/Popupdelet.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import attention from "../assets/attention.svg";
+import Button from "./button";
+
+const Popupdelete = ({
+  isVisible = false,
+  onClose,
+  onConfirm,
+  title = "Êtes-vous sûr ?",
+  message = "",
+  userId = null,
+  confirmText = "Supprimer",
+  iconSrc = attention,
+  iconBgColor = "#FFF0F0",
+  confirmColor = "#F09C0A",
+}) => {
+  if (!isVisible) return null;
+
+  return (
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center  bg-opacity-50 px-2">
+      <div className="bg-white w-full max-w-[90%] sm:max-w-[500px] p-6 rounded-3xl shadow-2xl relative text-center">
+        
+        {/* Bouton de fermeture (X) */}
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-400 hover:text-black text-lg"
+        >
+          X
+        </button>
+
+        {/* Icône */}
+        <img
+          src={iconSrc}
+          alt="Icon"
+          className="
+          w-6 h-6            
+          sm:w-8 sm:h-8      
+          md:w-10 md:h-10    
+          lg:w-12 lg:h-12     
+          xl:w-14 xl:h-14    
+          mx-auto mb-4
+        "
+        />
+
+        {/* Titre */}
+        <p className="text-xl font-semibold text-gray-900 mb-2">
+          {title} {userId && <span className="text-black">#{userId}</span>}
+        </p>
+
+        {/* Message */}
+        {message && (
+          <p className="text-sm text-gray-600 mb-6">{message}</p>
+        )}
+
+        {/* Bouton unique de confirmation */}
+        <div className="flex justify-center">
+          <Button
+            text={confirmText}
+            bgColor={confirmColor}
+            onClick={() => {
+              if (onConfirm) onConfirm(userId);
+              onClose();
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Popupdelete;
