5501ae6ea822293e652600e475a9a308c626b670
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-19 12:17:39 +0200

    Update writeContainer.jsx

--- src/components/writeContainer.jsx
+++ src/components/writeContainer.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function WriteContainer({ title, onSubmit }) {
+export default function WriteContainer({ title, onSubmit, onChange , value }) {
   const [input, setInput] = useState("");
+
+
+  useEffect(() => {
+    if (value !== undefined) {
+      setInput(value);
+    }
+  }, [value]);
 
   const handleChange = (e) => {
     setInput(e.target.value);
+    if (onChange) onChange(e.target.value); 
   };
 
   const handleBlur = () => {
@@ -12,7 +20,7 @@ export default function WriteContainer({ title, onSubmit }) {
   };
 
   return (
-    <div className="max-w-xs mx-auto">
+    <div className="w-full">
       <div className="mb-4">
         <label className="flex flex-col items-start text-sm font-poppins font-medium text-[#202124] text-[0.8125rem] mb-1 ml-0.25rem">
           {title}
@@ -24,7 +32,10 @@ export default function WriteContainer({ title, onSubmit }) {
             onChange={handleChange}
             onBlur={handleBlur}
             placeholder="---"
-            className="flex flex-col items-start truncate w-full py-3 px-4 border border-white rounded-[0.5rem] text-[#80868B] text-[0.8125rem] font-regular font-poppins bg-white resize-none focus:outline-0 focus:ring-0"
+           
+            className=" flex flex-col items-start w-full py-3 px-4 border border-white rounded-[0.5rem] text-[0.8125rem] font-regular font-poppins bg-white resize-none focus:outline-0 focus:ring-0 "
+           
+            
           />
         </div>
       </div>
