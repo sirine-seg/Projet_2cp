2adc6440c792f835c277cbc366157cc06baf25d1
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-04-19 11:52:35 +0100

    Update checked onCheck

--- src/components/filtreCheck.jsx
+++ src/components/filtreCheck.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-
 const FiltreCheck = ({ 
-    label,
-    }) => {
-  const [checked, setChecked] = useState(false);
-
+  label,
+  checked = false,
+  onCheck = () => {}
+}) => {
   return (
-    <label className="flex items-center space-x-3 max-w-[220px] cursor-pointer" onClick={() => setChecked(!checked)}>
+    <label
+      className="flex items-center space-x-3 max-w-[220px] py-2 cursor-pointer"
+      onClick={onCheck}
+    >
       <div
         className={`w-5 h-5 flex items-center justify-center rounded-md transition-colors duration-200 ${
-          checked
-            ? "bg-[#F09C0A]"
-            : "bg-white border-1 border-gray-400"
+          checked ? "bg-[#F09C0A]" : "bg-white border border-gray-400"
         }`}
       >
         {checked && (
@@ -29,6 +28,6 @@ const FiltreCheck = ({
       <span className="text-sm text-[#5F6368]">{label}</span>
     </label>
   );
-}
+};
 
 export default FiltreCheck;
