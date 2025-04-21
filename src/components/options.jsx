6de7194af59e0c37fbc9cec8af6a4c58522b3621
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-21 13:05:07 +0200

    Update options.jsx

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -1,23 +1,41 @@
-export default function Options({options = [], handleSelect}){
-    return (
-        <div className="w-auto left-0 right-0 mt-0.5 bg-white border border-gray-200 rounded-[0.5rem] shadow-xl py-1 overflow-auto text-[0.8125rem] font-poppins font-regular">
-            {options.map((option) => (
-              <div
-                key={option.value || option}
-                className="px-4 py-2 cursor-pointer transition-colors duration-200"
-                onClick={() => handleSelect(option.value || option)}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.backgroundColor = "#E1E3E4";
-                  e.currentTarget.style.color = "#202124";
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.backgroundColor = "white";
-                  e.currentTarget.style.color = "#202124";
-                }}
-              >
-                {option.label || option}
-              </div>
-            ))}
-          </div>
-    )
+import { useEffect, useRef } from "react";
+
+export default function Options({ options = [], handleSelect, setMenuOpen }) {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(null); // Fermer si clic en dehors
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [setMenuOpen]);
+
+  return (
+    <div
+      ref={menuRef}
+      className="absolute top-12 right-3 bg-white shadow-xl rounded-lg text-black w-48 sm:w-56 z-50 border"
+    >
+      {options.map((option) => (
+        <div
+          key={option.value || option}
+          className="px-4 py-2 cursor-pointer transition-colors duration-200"
+          onClick={() => handleSelect(option.value || option)}
+          onMouseEnter={(e) => {
+            e.currentTarget.style.backgroundColor = "#E1E3E4";
+            e.currentTarget.style.color = "#202124";
+          }}
+          onMouseLeave={(e) => {
+            e.currentTarget.style.backgroundColor = "white";
+            e.currentTarget.style.color = "#202124";
+          }}
+        >
+          {option.label || option}
+        </div>
+      ))}
+    </div>
+  );
 }
