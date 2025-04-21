9ad9c4dba734a027df1ae4201388994e03e6e5f0
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-21 15:42:58 +0200

    Update options.jsx

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -1,46 +1,23 @@
-import { useEffect, useRef } from "react";
-
-export default function Options({ options = [], handleSelect, setMenuOpen, isActive }) {
-  const menuRef = useRef(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (isActive && menuRef.current && !menuRef.current.contains(event.target)) {
-        setMenuOpen(null); // Fermer si clic en dehors ET que le menu est actif
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [setMenuOpen, isActive]);
-
-  return (
-    <div
-      ref={menuRef}
-      className="absolute top-12 right-3 bg-white shadow-xl rounded-lg text-black w-48 sm:w-56 z-50 border"
-    >
-      {options.map((option) => (
-        <div
-          key={option.value || option}
-          className="px-4 py-2 cursor-pointer transition-colors duration-200"
-          onClick={() => {
-            handleSelect(option.value || option);
-            if (isActive) {
-              setMenuOpen(null); // Fermer le menu seulement s'il est actif
-            }
-          }}
-          onMouseEnter={(e) => {
-            e.currentTarget.style.backgroundColor = "#E1E3E4";
-            e.currentTarget.style.color = "#202124";
-          }}
-          onMouseLeave={(e) => {
-            e.currentTarget.style.backgroundColor = "white";
-            e.currentTarget.style.color = "#202124";
-          }}
-        >
-          {option.label || option}
-        </div>
-      ))}
-    </div>
-  );
+export default function Options({options = [], handleSelect , className = "" }){
+    return (
+    className={`absolute w-full z-10 bg-white border border-gray-200 rounded-[0.5rem] shadow-xl py-1 max-h-60 overflow-auto text-[0.8125rem] font-poppins font-regular ${className}`}
+            {options.map((option) => (
+              <div
+                key={option.value || option}
+                className="px-4 py-2 cursor-pointer transition-colors duration-200"
+                onClick={() => handleSelect(option.value || option)}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.backgroundColor = "#E1E3E4";
+                  e.currentTarget.style.color = "#202124";
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.backgroundColor = "white";
+                  e.currentTarget.style.color = "#202124";
+                }}
+              >
+                {option.label || option}
+              </div>
+            ))}
+          </div>
+    )
 }
