53f49a96f14c3dc4590c542eef0c01ea051ea7e1
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-24 08:58:10 +0200

    Update equipModifierPopUp.jsx

--- src/components/equipModifierPopUp.jsx
+++ src/components/equipModifierPopUp.jsx
@@ -15,7 +15,7 @@ export default function EquipModifierPopUp({
     );
   
     return (
-      <div className="fixed inset-0 bg-black/50 flex items-center justify-center p-4">
+      <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
         <div className="bg-white rounded-[1rem] shadow-xl max-w-xl max-h-[62vh] flex flex-col overflow-hidden lg:min-w-[30rem]">
         
           <div className="flex justify-between items-center px-4 py-3">
