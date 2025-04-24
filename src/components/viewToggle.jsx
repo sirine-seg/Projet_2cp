776031cfce1e24af7d051697b5e85bda49dd648c
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-04-24 22:19:25 +0100

    Update viewToggle.jsx

--- src/components/viewToggle.jsx
+++ src/components/viewToggle.jsx
@@ -3,7 +3,7 @@ import viewList from '../assets/viewList.svg';
 import viewCard from '../assets/viewCard.svg';
 
 const ViewToggle = ({ onChange }) => {
-  const [view, setView] = useState("list");
+  const [view, setView] = useState("grid");
 
   const handleClick = (newView) => {
     setView(newView);
@@ -11,19 +11,19 @@ const ViewToggle = ({ onChange }) => {
   };
 
   return (
-    <div className="flex border-2 border-[#20599E] rounded-xl overflow-hidden w-fit shadow-sm">
+    <div className="flex border-2 border-[#20599E] rounded-xl overflow-hidden w-fit shadow-sm h-10">
       {/* Vue Liste */}
       <button
         onClick={() => handleClick("list")}
         className={`flex items-center justify-center transition-colors duration-200 
-          focus:outline-none focus:ring-2 focus:ring-[#20599E]
-          px-2 py-1
+          focus:outline-none focus:ring-1 focus:ring-[#20599E]
+          px-1 py-0.5
           ${view === "list" ? "bg-blue-100" : "bg-white"}`}
       >
         <img 
           src={viewList} 
           alt="List view" 
-          className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7" 
+          className="w-6 h-6 sm:w-7 sm:h-7" 
         />
       </button>
 
@@ -31,14 +31,14 @@ const ViewToggle = ({ onChange }) => {
       <button
         onClick={() => handleClick("grid")}
         className={`flex items-center justify-center transition-colors duration-200 
-          focus:outline-none focus:ring-2 focus:ring-[#20599E]
-          px-2 py-1
+          focus:outline-none focus:ring-1 focus:ring-[#20599E]
+          px-1 py-0.5
           ${view === "grid" ? "bg-blue-100" : "bg-white"}`}
       >
         <img 
           src={viewCard} 
           alt="Card view" 
-          className="w-5 h-5 sm:w-7 sm:h-7 md:w-8 md:h-8" 
+          className="w-6 h-6 sm:w-7 sm:h-7" 
         />
       </button>
     </div>
