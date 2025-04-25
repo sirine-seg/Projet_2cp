ce0bb931c3fa4a539d93f24e5403a5ef54fdcc7d
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-04-25 18:46:32 +0100

    colors variable

--- src/components/tabSelector.jsx
+++ src/components/tabSelector.jsx
@@ -5,22 +5,32 @@ const TabSelector = ({
   activeOption,
   setActiveOption,
   activeColor = '#FFFFFF',
-  notActiveColor = '#5F6368',
-  textSize = 'md:text-md', //it was originally text-lg but changed to md for notifsCard
+  inactiveColor = '#FFFFFF',
+  underlineColor = '#FFFFFF',
+  textSize = 'md:text-md',
 }) => {
   return (
     <div className="flex justify-center gap-3 sm:gap-6 md:gap-8 overflow-x-auto">
       {options.map((option, index) => (
         <button
           key={index}
-          onClick={() => setActiveOption(option)}
-          className={`relative text-sm sm:text-base ${textSize} font-medium pb-1 transition-all duration-200 whitespace-nowrap`}
+          onClick={() => setActiveOption(option.label)}
+          className={`relative text-sm sm:text-base ${textSize} font-medium pb-1 transition-all duration-200 whitespace-nowrap flex items-center gap-1`}
           style={{
-            color: activeOption === option ? activeColor : 'rgba(255,255,255,0.7)',
-            borderBottom: activeOption === option ? `2px solid ${activeColor}` : '2px solid transparent',
+            color: activeOption === option.label ? activeColor : inactiveColor,
+            borderBottom: activeOption === option.label
+              ? `2px solid ${underlineColor}`
+              : '2px solid transparent',
           }}
         >
-          {option}
+          <div className="flex items-center justify-center gap-x-2">
+            <span>{option.label}</span>
+            {option.count > 0 && (
+              <span className="w-5 h-5 bg-[#DFDFDF] text-[#5F6368] text-xs flex items-center justify-center rounded-full">
+                {option.count > 9 ? "9+" : option.count}
+              </span>
+            )}
+          </div>
         </button>
       ))}
     </div>
