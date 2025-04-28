ab5b90d1ce427a9a772906f5243f0e550d722a88
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-28 02:30:44 +0200

    Update addMobile.jsx

--- src/components/addMobile.jsx
+++ src/components/addMobile.jsx
@@ -1,20 +1,20 @@
 import Plus from "../assets/plus.svg";
 import { motion } from "framer-motion";
 
-export default function AddMobile ({onClick}) {
+export default function AddMobile({ onClick }) {
   return (
-      <div className="relative group">
-        <motion.button
+    <div className="fixed bottom-6 right-6 z-50 group">
+      <motion.button
         whileTap={{ scale: 0.9 }}
         onClick={onClick}
-        className="flex items-center justify-center w-16 h-16 bg-amber-500 text-white rounded-full focus:outline-none cursor-pointer active:scale-75 transition-transform duration-100
-">
-          <img
-            src={Plus}
-            alt="Plus"
-            className="mt-1"
-          />
-        </motion.button>
-      </div>
+        className="flex items-center justify-center w-16 h-16 bg-amber-500 text-white rounded-full focus:outline-none cursor-pointer active:scale-75 transition-transform duration-100 shadow-lg"
+      >
+        <img
+          src={Plus}
+          alt="Plus"
+          className="mt-1"
+        />
+      </motion.button>
+    </div>
   );
 }
