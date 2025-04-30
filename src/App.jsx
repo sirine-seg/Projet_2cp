f4e02ac2af9e28a10d40ad2482606cc1b05176fc
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2025-04-30 03:24:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Profil from './pages/profil.jsx';
 import Users from './utilisateurs/users.jsx';
+import Ajout from './assets/ajouttt.jsx';
+import TechnicienDetails from "./assets/technicien";
+import Modifie from "./assets/modifier";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/Profil" element={<Profil />} />
         <Route path="/Users" element={<Users />} />
+         <Route path="/Modifie/:id" element={< Modifie/>} />
+          <Route path="/Ajout" element={< Ajout />} />
+         <Route path="/TechnicienDetails/:id" element={< TechnicienDetails />} />
       </Routes>
     </BrowserRouter>
   );
