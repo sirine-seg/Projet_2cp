aef48e2bdded356876f68c9eb3a1047080303cbe
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-05-01 12:32:17 +0100

    Update signalRaccourci.jsx

--- src/components/signalRaccourci.jsx
+++ src/components/signalRaccourci.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import Alert from "../assets/allert.svg";
 
 export default function SignalRaccourci() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/Ajoutintervention");
+  };
+
   return (
     <div className="flex justify-center">
       <div className="rounded-xl w-full bg-[#20599E] p-4 sm:p-6 shadow-[10px_10px_30px_-10px_rgba(32,33,36,0.7)]">
@@ -20,10 +28,26 @@ export default function SignalRaccourci() {
             </h3>
             <h3 className="text-lg md:text-xl lg:text-2xl font-semibold sm:font-bold text-white leading-tight mb-2">Signalez le!</h3>
             </div>
-            <button className="bg-[#F09C0A] text-white text-sm sm:text-base font-semibold py-1.5 px-5 rounded-full transition-colors duration-200 w-fit cursor-pointer">
+              <motion.button
+                onClick={handleClick}
+                className="bg-[#F09C0A] text-white text-sm sm:text-base font-semibold py-1.5 px-5 rounded-full w-fit cursor-pointer"
+                whileHover={{
+                  scale: 1.02,
+                  backgroundColor: "#e6910a",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"
+                }}
+                whileTap={{
+                  scale: 0.9,
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)"
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10
+                }}
+            >
               Signaler un équipement
-            </button>
-            
+            </motion.button>
           </div>
         </div>
       </div>
