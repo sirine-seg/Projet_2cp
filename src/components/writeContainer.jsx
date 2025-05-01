f8b95e7c55e4045000a7de0c24e95cd472b9aab1
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-05-01 15:05:58 +0100

    Added message

--- src/components/writeContainer.jsx
+++ src/components/writeContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function WriteContainer({ title, onSubmit, onChange , value, bgColor="bg-[#FFFFFF]" }) {
+export default function WriteContainer({ title, onSubmit, onChange , value , message }) {
   const [input, setInput] = useState("");
 
 
@@ -22,21 +22,27 @@ export default function WriteContainer({ title, onSubmit, onChange , value, bgCo
   return (
     <div className="w-full">
       <div className="mb-4">
-        <label className="flex flex-col items-start text-sm font-poppins font-medium text-[#202124] text-[0.8125rem] mb-1 ml-0.25rem">
+      
+        <label className="flex flex-col items-start text-sm font-poppins font-medium text-[#202124] text-[0.8125rem] mb-1 ml-0.25rem">      
           {title}
         </label>
+
+        {message && (
+          <div className="text-xs text-gray-500 mb-1 ml-0.25rem">
+            {message}
+          </div>
+        )}
 
         <div className="relative mt-0.25rem">
           <input
             value={input}
             onChange={handleChange}
             onBlur={handleBlur}
-            placeholder="---"           
-            className={`flex flex-col items-start w-full py-3 px-4 border border-white rounded-[0.5rem] text-[0.8125rem] font-regular font-poppins ${bgColor} resize-none focus:outline-0 focus:ring-0`}
-           
-            
+            placeholder="---"
+            className=" flex flex-col items-start w-full py-3 px-4 border border-white rounded-[0.5rem] text-[0.8125rem] font-regular font-poppins bg-white resize-none focus:outline-0 focus:ring-0 "
           />
         </div>
+        
       </div>
     </div>
   );
