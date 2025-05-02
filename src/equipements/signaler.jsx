40448015a154e2d6d6e36b407e511d9b3ddbb129
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-05-02 09:12:22 +0100

    Update signaler.jsx

--- src/equipements/signaler.jsx
+++ src/equipements/signaler.jsx
@@ -283,7 +283,7 @@ const urgenceOptions = [
 console.log("Techniciens disponibles :", techniciensDispo); // 👀
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center bg-[#20599E] rounded-r-md  overflow-hidden">
+    <div className="w-full min-h-screen flex flex-col items-center bg-[#20599E] overflow-hidden">
 
                     
     {/* Logo en haut à gauche */}
