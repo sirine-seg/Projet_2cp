7010647e35e1b1294fe0c88f9c99116b59d2e32d
Sirine <154369857+sirine-seg@users.noreply.github.com>
2025-05-02 23:49:20 +0100

    Update importManual.jsx

--- src/components/importManual.jsx
+++ src/components/importManual.jsx
@@ -1,24 +1,6 @@
-// import Cloud from "../assets/Cloud.svg";
-
-// export function ImportManual({ onClick }) {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className="flex items-center gap-4 px-4 py-3 bg-white text-[#80868B] text-sm font-semibold rounded-lg transition-colors shadow-sm"
-//     >
-//       <span>Importer le manuel</span>
-//       <img src={Cloud} alt="Cloud " className="w-5 h-5" />
-
-//     </button>
-//   );
-// }
-
-// // Usage example:
-// // <ImportManualButton onClick={() => console.log('Import clicked')} />
-
 import Cloud from "../assets/Cloud.svg";
 
-export function ImportManual({ onClick }) {
+export default function ImportManual({ onClick }) {
   return (
     <button
       onClick={onClick}
