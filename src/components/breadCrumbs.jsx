2fcceba61aa2678bf34c24c6b7b9205bec153778
Imene <154606844+ImeneTouadi@users.noreply.github.com>
2025-05-02 16:36:00 +0200

    made it responsive

--- src/components/breadCrumbs.jsx
+++ src/components/breadCrumbs.jsx
@@ -1,16 +1,15 @@
 export default function Breadcrumb({ path = [] }) {
-    return (
-      <div className="flex flex-col">
-        <div className="flex items-center text-[#202124] text-3xl font-medium space-x-1 pb-4 ml-8">
-          {path.map((item, index) => (
-            <div key={index} className="flex items-center">
-              <span>{item}</span>
-              {index < path.length - 1 && <span className="mx-1">/</span>}
-            </div>
-          ))}
-        </div>
-        <div className="border-b border-gray-300 max-w-2xl" />
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-wrap items-center text-[#202124] text-xl sm:text-3xl font-medium space-x-1 pb-4 ml-2 sm:ml-8">
+        {path.map((item, index) => (
+          <div key={index} className="flex items-center">
+            <span className="truncate max-w-[200px] sm:max-w-full">{item}</span>
+            {index < path.length - 1 && <span className="mx-1">/</span>}
+          </div>
+        ))}
       </div>
-    );
-  }
-  
+      <div className="border-b border-gray-300 max-w-full sm:max-w-2xl" />
+    </div>
+  );
+}
