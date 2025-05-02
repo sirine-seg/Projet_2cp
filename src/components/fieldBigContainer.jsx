515f78b3760e9147c628ed912654010f382f5211
Imene <154606844+ImeneTouadi@users.noreply.github.com>
2025-05-02 16:37:40 +0200

    made it responsive

--- src/components/fieldBigContainer.jsx
+++ src/components/fieldBigContainer.jsx
@@ -20,23 +20,23 @@ export default function FieldBigContainer({
     <div className="w-full">
       <div className="relative w-full rounded-md shadow-xs px-4 py-2 flex items-center justify-between bg-white">
         <h2 className="ml-2 text-md font-semibold text-[#202124]">{field}</h2>
-        <div className="flex items-center gap-1">
+        <div className="flex items-center sm:gap-1">
           <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             onClick={onClickAdd}
             className="p-1.5 rounded-full cursor-pointer flex items-center justify-center"
           >
-            <img src={addBlack} alt="add" className="h-6 w-6" />
+            <img src={addBlack} alt="add" className="h-5 w-5 sm:h-6 sm:w-6" />
           </motion.button>
           <button
             onClick={toggleOpen}
             className="p-1.5 rounded-full cursor-pointer transition-transform"
           >
             {isOpen ? (
-              <ChevronUp className="w-6 h-6" />
+              <ChevronUp className="h-5 w-5 sm:h-6 sm:w-6" />
             ) : (
-              <ChevronDown className="w-6 h-6" />
+              <ChevronDown className="h-5 w-5 sm:h-6 sm:w-6" />
             )}
           </button>
         </div>
