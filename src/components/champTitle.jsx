c2094b305a1ecd406833669fdcb51de30960aa7c
Imene <154606844+ImeneTouadi@users.noreply.github.com>
2025-05-02 16:36:45 +0200

    made it responsive

--- src/components/champTitle.jsx
+++ src/components/champTitle.jsx
@@ -1,14 +1,29 @@
 import AjouterButton from "./Ajouterbutton";
 import { ChevronRight } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
+import addBlack from "../assets/addBlack.svg";
 
-export default function ChampTitle ({title, handleAjouterClick}){
-    return (
-        <div className="w-full flex items-center justify-between mb-8">
-          <div className="flex items-center gap-2">
-            <ChevronRight size={18} />
-            <h1 className="text-2xl font-semibold">{title}</h1>
-          </div>
-          <AjouterButton text="Ajouter" onClick={handleAjouterClick} />
-        </div>
-    )
+export default function ChampTitle({ title, handleAjouterClick }) {
+  return (
+    <div className="w-full flex items-center justify-between mb-8">
+      <div className="flex items-center gap-2 flex-1 min-w-0">
+        <ChevronRight size={18} className="flex-shrink-0" />
+        <h1 className="text-lg sm:text-3xl font-semibold">{title}</h1>
+      </div>
+
+      <div className="block md:hidden">
+      <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={handleAjouterClick}
+            className="p-1.5 rounded-full cursor-pointer flex items-center justify-center"
+          >
+            <img src={addBlack} alt="add" className="h-6 w-6" />
+          </motion.button>
+      </div>
+      <div className="hidden md:block">
+        <AjouterButton text="Ajouter" onClick={handleAjouterClick} />
+      </div>
+    </div>
+  );
 }
